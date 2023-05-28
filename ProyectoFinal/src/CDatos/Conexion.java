d55be7b30babcf7eedd60be449eefadb3abf3bca
David Timo <josedtl115@gmail.com>
2023-05-28 17:33:57 -0500

    Cambio TOTAL

--- ProyectoFinal/src/CDatos/Conexion.java
+++ ProyectoFinal/src/CDatos/Conexion.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.  DAVISD ESTUBO AQUI
+ * and open the template in the editor. entra maquina
  */
 
 package CDatos;
